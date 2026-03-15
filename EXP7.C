0c65e91d445e2e0b9598a0c24d13f3586c66f24d
saranvicky2807-coder <saranvicky2807@gmail.com>
2026-03-15 20:29:29 +0530

    Create EXP7.C

---
+++ EXP7.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    if(num % 2 == 0)
+        printf("Even Number");
+    else
+        printf("Odd Number");
+
+    return 0;
+}
